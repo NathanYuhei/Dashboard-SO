9c5950a8b7605e8c8fb2ca59725badb4a1cb0d43
Nathan Yuhei <67643939+NathanYuhei@users.noreply.github.com>
2023-10-26 21:14:46 -0300

    Update model.py

--- model.py
+++ model.py
@@ -13,21 +13,8 @@ import pwd
 
 class Model:
 
-    """
     @staticmethod
-    def getCpuInfo():
-        info = {}
-        with open('/proc/cpuinfo') as meminfo_file:
-            for line in meminfo_file:
-                parts = line.split(':')
-                if len(parts) == 2:
-                    key = parts[0].strip()
-                    value = parts[1].strip().split()[0]
-                    info[key] = value
-        return info
-
-    @staticmethod
-    def getMemoryInfo():
+    def get_memory_info():
         info = {}
         with open('/proc/meminfo') as meminfo_file:
             for line in meminfo_file:
@@ -35,10 +22,37 @@ class Model:
                 if len(parts) == 2:
                     key = parts[0].strip()
                     value = parts[1].strip().split()[0]
-                    info[key] = (int(value) / 1024) / 1024
+                    info[key] = value
 
         return info
-    """
+
+    def get_memory_percent_used(self):
+        data = self.get_memory_info()
+        mem_used_percent = ((float(data['MemTotal']) - float(data['MemFree'])) / float(data['MemTotal'])) * 100
+
+        percent_used = "Memória Usada: " + str(int(mem_used_percent)) + '%'
+
+        return percent_used
+
+    def get_memory_percent_free(self):
+        data = self.get_memory_info()
+        percent_free = int((float(data['MemFree']) / float(data['MemTotal'])) * 100)
+        final_data = "Memória Livre: " + str(percent_free) + '%'
+        return final_data
+
+    def get_memory_total_RAM(self):
+        data = self.get_memory_info()
+        memTotal = (float(data['MemTotal']) / 1024) / 1024
+        final_data = 'Memória Física: ' + str(int(memTotal)) + 'GB'
+
+        return final_data
+
+    def get_memory_total_virtual(self):
+        data = self.get_memory_info()
+        vmTotal = (float(data['VmallocTotal']) / 1024) / 1024
+        final_data = "Memória Virtual: " + str(int(vmTotal)) + 'GB'
+
+        return final_data
 
     def get_cpu_usage(self):
         total_time = 0
@@ -74,7 +88,7 @@ class Model:
                     pass
         return thread_count
 
-    def get_processes(self):
+        def get_processes(self):
         processes = []
         for pid in os.listdir('/proc'):
             if pid.isdigit():
@@ -82,13 +96,28 @@ class Model:
                     with open(f'/proc/{pid}/status') as f:
                         name = ''
                         user = ''
+                        vmPeak = ''
+                        vmSize = ''
+                        vmExe = ''
+                        vmStk = ''
                         for line in f:
                             if 'Name' in line:
                                 name = line.split()[1]
-                            if 'Uid' in line:
+                            elif 'Uid' in line:
                                 uid = line.split()[1]
                                 user = pwd.getpwuid(int(uid)).pw_name
-                        processes.append((int(pid), name, user))
+                            elif 'VmPeak' in line:
+                                vmPeak = line.split()[1]
+                            elif 'VmSize' in line:
+                                vmSize = line.split()[1]
+                            elif 'VmExe' in line:
+                                vmExe = line.split()[1]
+                            elif 'VmStk' in line:
+                                vmStk = line.split()[1]
+
+
+
+                        processes.append((int(pid), name, user, vmPeak, vmSize, vmStk, vmExe))
                 except FileNotFoundError:
                     pass
         return processes
